8944e00b566461cb8c53ba492390f102f9b9a490
khojwar <khojwartikaram@gmail.com>
2024-12-29 22:29:37 +0545

    adding js basics

---
+++ 4.1 js intro/script.js
@@ -0,0 +1,11 @@
+// to run this code use node.js or any other javascript engine that supports ES6 syntax and modules 
+// type "node script.js" in the terminal to run the code 
+// or use an online javascript compiler like https://www.jdoodle.com/console-nodejs to run the code 
+
+
+console.log("Hello, World!");
+
+
+
+
+
---
+++ 4.2 Embeeding JavaScript to a webpage/script.js
@@ -0,0 +1,9 @@
+// console.log("hello bca 5");
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "after external js";
+}
+
+
+
+
